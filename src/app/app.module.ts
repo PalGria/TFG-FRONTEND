7d881b5bf1b69f3eb0db05d8586fe1d504e1c111
Thesebio <josemanuel@paroli.es>
2018-08-19 14:08:46 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MynavComponent } from './mynav/mynav.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MynavComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
